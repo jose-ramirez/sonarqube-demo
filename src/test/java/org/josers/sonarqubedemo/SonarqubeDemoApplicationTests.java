a010bbb63b64816dfab3e7aa5dad0dda8b6b4901
Jose Ramirez <a30673083@gmail.com>
2022-02-16 21:15:51 -0300

    Project base

---
+++ src/test/java/org/josers/sonarqubedemo/SonarqubeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.josers.sonarqubedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
